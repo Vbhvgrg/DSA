42819511a9c47cb807d097146c669b1306ba01c3
Vbhvgrg <86735014+Vbhvgrg@users.noreply.github.com>
2023-03-29 21:04:09 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,36 @@
+// Binary Search;;
+
+#include <iostream>
+
+using namespace std;
+
+int BS(int arr[],int start,int end,int ele){
+    
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        
+        if(ele==arr[mid]){
+            return mid;
+        }else if(ele<arr[mid]){
+            end=mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+    
+    return -1;
+    
+}
+
+int main()
+{
+   int arr[]={1,4,7,9,10,12};
+   
+   int start=0;
+   int end=5;
+   
+   int Element=12;
+   
+   int index=BS(arr,start,end,Element);
+   cout<<index;
+}
