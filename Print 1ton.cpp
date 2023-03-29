d4122784983a27f6dab4a507af107b02cd444e92
Vbhvgrg <86735014+Vbhvgrg@users.noreply.github.com>
2023-03-29 20:57:31 +0530

    Create Print 1ton.cpp

---
+++ Print 1ton.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+void print(int n){
+    if(n==1){
+        cout<<n<<" ";
+        return;
+    }
+    
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main()
+{
+    print(7);
+
+    return 0;
+}
